e7e34d0b37fbc1539881471b46eaa31718bc86d1
Aashrit17 <aashrit17038gmail.comgit config --global user.name Aashrit17>
2024-02-25 15:55:31 +0545

    initial commit

---
+++ lib/viewmodels/category_viewmodel.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:time_keepers_mobile/models/category_model.dart';
+import 'package:time_keepers_mobile/models/user_model.dart';
+import 'package:time_keepers_mobile/repositories/auth_repositories.dart';
+import 'package:time_keepers_mobile/services/firebase_service.dart';
+import 'package:time_keepers_mobile/viewmodels/global_ui_viewmodel.dart';
+
+import '../repositories/category_repositories.dart';
+
+class CategoryViewModel with ChangeNotifier {
+  CategoryRepository _categoryRepository = CategoryRepository();
+  List<CategoryModel> _categories = [];
+  List<CategoryModel> get categories => _categories;
+  Future<void> getCategories() async {
+    _categories = [];
+    try {
+      var response = await _categoryRepository.getCategories();
+      for (var element in response) {
+        print(element.id);
+        _categories.add(element.data());
+      }
+      notifyListeners();
+    } catch (e) {
+      print(e);
+      _categories = [];
+      notifyListeners();
+    }
+  }
+}
